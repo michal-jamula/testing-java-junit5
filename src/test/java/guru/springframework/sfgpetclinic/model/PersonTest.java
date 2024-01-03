ccad2e193865bd270ebcdaea5298730339fdbb1a
michaldesktop <michal5832@gmail.com>
2024-01-03 17:11:22 +0000

    grouped assertions example

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,35 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void groupedAssertions() {
+        //given
+        Person person = new Person(11L, "Joe", "Buck");
+        //then
+        assertAll("Test properties set Set",
+                () -> assertEquals("Joe", person.getFirstName(), "Person's first name failed"),
+                () -> assertEquals( "Buck", person.getLastName(),"Person's last name failed"));
+
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void setFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void setLastName() {
+    }
+}
