2b51e3778d0a10e6edbe422283a8ec25f8bb875e
michaldesktop <michal5832@gmail.com>
2024-01-03 17:27:21 +0000

    JUnit Dependent Assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        owner.setCity("Key West");
+        owner.setTelephone("123123123");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties",
+                        () -> assertEquals("Joe", owner.getFirstName(), "First Name did not match"),
+                        () -> assertEquals("Buck", owner.getLastName(), "Last Name did not match")),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Key West", owner.getCity(), "City did not match"),
+                        () -> assertEquals("123123123", owner.getTelephone(), "Telephone did not match"))
+        );
+    }
+
+}
