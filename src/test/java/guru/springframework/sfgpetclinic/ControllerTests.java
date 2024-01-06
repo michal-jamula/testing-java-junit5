253d910959fa03c07c8d52807420b9d3e09056af
michaldesktop <michal5832@gmail.com>
2024-01-06 18:56:30 +0000

    JUnit 5 interfaces

---
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Tag;
+
+@Tag("controller")
+public interface ControllerTests {
+}
+
+//JUnit 5 picks up test properties so you can implement this interface with any JUnit test class.
--- src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.ControllerTests;
 import guru.springframework.sfgpetclinic.fauxspring.Model;
 import guru.springframework.sfgpetclinic.fauxspring.ModelMapTestImpl;
 import guru.springframework.sfgpetclinic.model.Vet;
@@ -8,13 +9,15 @@ import guru.springframework.sfgpetclinic.services.VetService;
 import guru.springframework.sfgpetclinic.services.map.SpecialityMapService;
 import guru.springframework.sfgpetclinic.services.map.VetMapService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import javax.naming.ldap.Control;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class VetControllerTest {
+class VetControllerTest implements ControllerTests {
 
     private VetService vetService;
     SpecialtyService specialtyService;
